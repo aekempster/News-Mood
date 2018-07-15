6ff4a0853001d24aa99c1b153ef4a01d2c4ca827
Annabelle Kempster <annabelle.kempster@gmail.com>
2018-07-15 13:30:45 -0400

    pushing all files

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "4D5RjkDrY7R4iPBN0QXpfxper"
+consumer_secret = "tyEzhLKXZufDBB6sfdGHDHibmkaK6jgWdscf169w3tEidoyTFW"
+access_token = "35690169-CGXs4Muh8My16evFc4rHes4sE6zITIwu0ADuEtIVQ"
+access_token_secret = "K0iRUtWIK2wYy0GEVwGI1AnLOFmoXCKJav1XX4I1VkZF9"
